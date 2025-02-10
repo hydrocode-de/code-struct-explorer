54346c0cec078a104739f9367584fbc05edbc25d
mmaelicke <mirko@hydrocode.de>
2025-02-10 09:44:33 +0100

    try to set base path directly

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,5 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  paths: {
-    base: process.argv.includes('dev') ? '' : '/code-struct-explorer'
-  }
+  preprocess: vitePreprocess()
 }
